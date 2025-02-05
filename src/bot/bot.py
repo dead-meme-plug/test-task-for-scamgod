b29e289b25a8a88e273d9320d97563af69f0082f
Mister ze <mrz@gmail.com>
2025-02-05 18:39:28 +0300

    хуйня для фонового фетчинга новостей + ее интеграция

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,5 +1,8 @@
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import BotCommand
+
+from src.bot.handlers import commands, callbacks
 
 class TGBot:
     def __init__(self, BOT_API_TOKEN: str):
@@ -7,7 +10,14 @@ class TGBot:
         self.dp = Dispatcher(storage=MemoryStorage())
         
     def _setup_handlers(self):
-        self.dp.include_routers()
+        self.dp.include_router(commands.router)
+        self.dp.include_router(callbacks.router)
         
     async def start(self):
+        await self._set_bot_commands()
         await self.dp.start_polling(self.bot)
+
+    async def _set_bot_commands(self):
+        await self.bot.set_my_commands([
+            BotCommand(command="/start", description="Начать работу с ботом")
+        ])
