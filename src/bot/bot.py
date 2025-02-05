2a7c7189a08cbeec874a150ffe6b753a8168e73b
Mister ze <mrz@gmail.com>
2025-02-05 19:27:47 +0300

    нормальная передача api fetcher

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -2,12 +2,16 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import BotCommand
 
-from src.bot.handlers import commands, callbacks
+from services.news_fetcher import NewsFetcher
+from services.api_fetcher import APIFetcher
+from bot.handlers import commands, callbacks
 
 class TGBot:
     def __init__(self, BOT_API_TOKEN: str):
         self.bot = Bot(token=BOT_API_TOKEN)
         self.dp = Dispatcher(storage=MemoryStorage())
+        self.api_fetcher = APIFetcher()
+        self.news_fetcher = NewsFetcher(self.api_fetcher, self.bot)
         
     def _setup_handlers(self):
         self.dp.include_router(commands.router)
@@ -15,6 +19,7 @@ class TGBot:
         
     async def start(self):
         await self._set_bot_commands()
+        await self.news_fetcher.start()
         await self.dp.start_polling(self.bot)
 
     async def _set_bot_commands(self):
